6bcfbc3e111ff474c3502bac6071b9b37a253c1d
Shubhro Banerjee <sbanerjee@shockwavemedical.com>
2025-04-08 16:49:26 +0530

    search is working

--- operation_metric_app.py
+++ operation_metric_app.py
@@ -13,10 +13,24 @@ st.set_page_config(layout = 'wide')
 SUCCESS_MSG = "Records updated successfully!"
 ERROR_MSG = "Error updating records. Please try again."
 
+
 def get_metric_data():
     query = f"SELECT * FROM STAGING_DEV.OPERATION_METRICS.OPERATION_METRIC_DETAILS"
     result = session.sql(query).collect()
     return result
+    
+#setups
+current_table_data = session.create_dataframe(get_metric_data())
+
+if 'open_to_edit_df' not in st.session_state:
+    st.session_state.open_to_edit_df = current_table_data.to_pandas()
+    
+if 'filtered_df' not in st.session_state:
+    st.session_state.filtered_df = current_table_data.to_pandas()
+
+if 'is_filtered' not in st.session_state:
+    st.session_state.is_filtered = False
+    
 
 def get_column_config():
     return {
@@ -37,89 +51,95 @@ def get_column_config():
         'FUTURE_1': st.column_config.TextColumn(label="FUTURE-1"),
         'FUTURE_2': st.column_config.TextColumn(label="FUTURE-2"),
         'METRIC_VALUE': st.column_config.TextColumn(label="METRIC VALUE"),
+        'METRIC_UOM': st.column_config.TextColumn(label="METRIC UOM"),
         'ENABLED_FLAG': st.column_config.TextColumn(label="ENABLED FLAG"),
         'COMMENTS': st.column_config.TextColumn(label="COMMENTS"),
-        'METRIC_UOM': st.column_config.TextColumn(label="METRIC UOM"),
+        'LAST_UPDATE_DATE': st.column_config.DatetimeColumn(label="LAST UPDATE DATE", disabled=True),
+        'LAST_UPDATED_BY': st.column_config.TextColumn(label="LAST UPDATED BY", disabled=True),
     }
 
 
-# merging the changes to original table
-def merge_data(dataset, updated_dataset):
-    try:
-        st.warning("Attempting to update dataset")
-        dataset.merge(
-            source=updated_dataset,
-            join_expr=(
-                (dataset["METRIC_NAME"] == updated_dataset["METRIC_NAME"])
-                & (dataset["METRIC_TYPE"] == updated_dataset["METRIC_TYPE"])
-                & (dataset["LOCATION"] == updated_dataset["LOCATION"])
-                & (dataset["TIME_PERIOD_TYPE"] == updated_dataset["TIME_PERIOD_TYPE"])
-                & (dataset["FUTURE_1"] == updated_dataset["FUTURE_1"])
-                & (dataset["FUTURE_2"] == updated_dataset["FUTURE_2"])
-                & (dataset["TIME_PERIOD_VALUE"] == updated_dataset["TIME_PERIOD_VALUE"])
-            ),
-            clauses=[
-                when_matched().update({
-                    col: updated_dataset[col] for col in updated_dataset.columns
-                    if col in dataset.columns
-                }),
-                when_not_matched().insert({
-                    col: updated_dataset[col] for col in updated_dataset.columns
-                    if col in dataset.columns
-                })
-            ]
-        )
-        st.success(SUCCESS_MSG)
-        time.sleep(.5)
-        # st.experimental_rerun()
+# # merging the changes to original table
+# def merge_data(dataset, updated_dataset):
+#     try:
+#         st.warning("Attempting to update dataset")
+#         dataset.merge(
+#             source=updated_dataset,
+#             join_expr=(
+#                 (dataset["METRIC_NAME"] == updated_dataset["METRIC_NAME"])
+#                 & (dataset["METRIC_TYPE"] == updated_dataset["METRIC_TYPE"])
+#                 & (dataset["LOCATION"] == updated_dataset["LOCATION"])
+#                 & (dataset["TIME_PERIOD_TYPE"] == updated_dataset["TIME_PERIOD_TYPE"])
+#                 & (dataset["FUTURE_1"] == updated_dataset["FUTURE_1"])
+#                 & (dataset["FUTURE_2"] == updated_dataset["FUTURE_2"])
+#                 & (dataset["TIME_PERIOD_VALUE"] == updated_dataset["TIME_PERIOD_VALUE"])
+#             ),
+#             clauses=[
+#                 when_matched().update({
+#                     col: updated_dataset[col] for col in updated_dataset.columns
+#                     if col in dataset.columns
+#                 }),
+#                 when_not_matched().insert({
+#                     col: updated_dataset[col] for col in updated_dataset.columns
+#                     if col in dataset.columns
+#                 })
+#             ]
+#         )
+#         st.success(SUCCESS_MSG)
+#         time.sleep(.5)
+#         # st.experimental_rerun()
         
-    except Exception as e:
-        st.error(f"{ERROR_MSG}: {e}")
-        st.stop()
+#     except Exception as e:
+#         st.error(f"{ERROR_MSG}: {e}")
+#         st.stop()
         
-current_table_data = session.create_dataframe(get_metric_data())
 
-# Execute the query and convert it into a Pandas dataframe
-open_to_edit_df = current_table_data.to_pandas()
 
-st.subheader("Operations Metric Details")
+st.subheader("Operations Metric Definition")
 search_col, add_row_col, col3 = st.columns([1,1, 3], gap="small", vertical_alignment="bottom")
 
+
+# if add_row_col.button("Add Row", type="primary",  icon=":material/add:"):
+#     # Adding a new row
+#     column_names = st.session_state.open_to_edit_df.columns.tolist()
+#     new_row = pd.DataFrame([{col: None for col in column_names}]) 
+#     # open_to_edit_df = pd.concat([new_row, open_to_edit_df], ignore_index=True)
+#     st.session_state.open_to_edit_df = pd.concat([new_row, st.session_state.open_to_edit_df], ignore_index=True)
+
+    
 with search_col:
     search_text = st.text_input("Search:")
     if search_text:
         # Add your search logic here using queried_data
-        open_to_edit_df = open_to_edit_df[
-            open_to_edit_df.apply(
+        st.session_state.is_filtered = True
+        st.session_state.filtered_df = st.session_state.open_to_edit_df[
+            st.session_state.open_to_edit_df.apply(
                 lambda row: any(
                     search_text.lower() in str(value).lower() for value in row.astype(str).values
                 ),
                 axis=1,
             )
         ]
+        st.write("Filtered", st.session_state.is_filtered, search_text)
+        
     else:
-        open_to_edit_df = current_table_data.to_pandas() # Display the full dataframe if no search
+        st.session_state.is_filtered = False
+        st.write("Not Filtered", st.session_state.is_filtered, search_text)
+        
 
-if add_row_col.button("Add Row", type="primary",  icon=":material/add:"):
-    # Adding a new row
-    column_names = open_to_edit_df.columns.tolist()
-    new_row = pd.DataFrame([{col: None for col in column_names}]) 
-    open_to_edit_df = pd.concat([new_row, open_to_edit_df], ignore_index=True)
-
-    
-
-with st.form("Operations Metric Defination"):
-    open_to_edit_df = st.data_editor(
-        data=open_to_edit_df,
+with st.form("Operations Metric Definition"):
+    st.session_state.edited_df = st.data_editor(
+        data= st.session_state.filtered_df if st.session_state.is_filtered else st.session_state.open_to_edit_df,
         column_config=get_column_config(),
         use_container_width=True,
         hide_index=True,
-        num_rows="dynamic"
+        # num_rows="dynamic"
     )
+
     submitted = st.form_submit_button("Submit Changes")
 
 
 if submitted:
-    updated_dataset = session.create_dataframe(open_to_edit_df)
+    updated_dataset = session.create_dataframe(st.session_state.open_to_edit_df)
     dataset = session.table("STAGING_DEV.OPERATION_METRICS.OPERATION_METRIC_DETAILS")
     merge_data(dataset, updated_dataset)
